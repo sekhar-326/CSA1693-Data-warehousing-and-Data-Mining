b16eb0760054fcb69fe12a8b15fc619c890bcaf8
sekhar-326 <113413355+sekhar-326@users.noreply.github.com>
2023-05-05 21:58:24 +0530

    write R Program to find Slicing Operations
    > s <-"sekhar"
    > print(substring(s,1,1))
    [1] "s"
    > print(substring(s,1,5))
    [1] "sekha"
    > my_string <- "Hello, world!"
    > 
    > slice <- substr(my_string, 1, 5)
    > print(slice)
    [1] "Hello"
    > 
    > df<-c("Alok","Joseph","Hayato","Kelly","Paloma","Moca")
    > substring(df,4,4)<-c("$")
    > df
    [1] "Alo$"   "Jos$ph" "Hay$to" "Kel$y"  "Pal$ma" "Moc$"  
    > 
    > 
    > df<-("Saveetha_school_of_Engineering")
    > substring(df,1,11)
    [1] "Saveetha_sc"

---
+++ Slicing Operations.R
@@ -0,0 +1,15 @@
+s <-"sekhar"
+print(substring(s,1,1))
+print(substring(s,1,5))
+my_string <- "Hello, world!"
+
+slice <- substr(my_string, 1, 5)
+print(slice)
+
+df<-c("Alok","Joseph","Hayato","Kelly","Paloma","Moca")
+substring(df,4,4)<-c("$")
+df
+
+
+df<-("Saveetha_school_of_Engineering")
+substring(df,1,11)
