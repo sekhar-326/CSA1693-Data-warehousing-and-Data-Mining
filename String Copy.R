667c49d36d371f2e327338a2fc7a580fc112eb00
sekhar-326 <113413355+sekhar-326@users.noreply.github.com>
2023-05-05 22:00:51 +0530

    write R Program for steing copy
     original_string <- "Welcome to SSE!!"
    > copy_string <- paste(original_string)
    > print(copy_string)
    [1] "Welcome to SSE!!"
    > 
    > original_string <- "Welcome to SSE world!!"
    > copy_string <- paste0(original_string)
    > print(copy_string)
    [1] "Welcome to SSE world!!"

---
+++ String Copy.R
@@ -0,0 +1,7 @@
+original_string <- "Welcome to SSE!!"
+copy_string <- paste(original_string)
+print(copy_string)
+
+original_string <- "Welcome to SSE world!!"
+copy_string <- paste0(original_string)
+print(copy_string)
