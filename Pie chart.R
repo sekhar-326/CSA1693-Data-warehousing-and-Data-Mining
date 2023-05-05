9f445a7016800a6972a56d915b51bf6cb26dae2a
sekhar-326 <113413355+sekhar-326@users.noreply.github.com>
2023-05-05 14:59:23 +0530

    write R Program for pie chart
    > a <- c(10,20,30,40,50,60)
    > a <- c(10,20,30,40,50,60)
    > result <- (pie(a,main="piechart",labels =c( "student 1","student 2","student 3","student 4","student 5","student 6"),
    +                     col = c("red", "orange", "yellow", "blue", "green","black")))
    > print(result)
    NULL

---
+++ Pie chart.R
@@ -0,0 +1,9 @@
+a <- c(10,20,30,40,50,60)
+result <- (pie(a,main="piechart",labels =c( "student 1","student 2","student 3","student 4","student 5","student 6"),
+                    col = c("red", "orange", "yellow", "blue", "green","black")))
+print(result)
+
+
+
+
+
